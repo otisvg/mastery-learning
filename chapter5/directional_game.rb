80388076cb756736c22eb0070d66a832193be7f7
Otis <otisvickersgraver@gmail.com>
2020-09-08 22:42:07 +0100

    Added refactored solution for learning purposes

--- chapter5/directional_game.rb
+++ chapter5/directional_game.rb
@@ -35,3 +35,28 @@ elsif move_1 == 'forward'
 else
   puts message
 end
+
+#REFACTORED SOLUTION TO COMPARE AND LEARN
+# 
+# move_number = 1
+#
+# while true do
+#   puts "You're facing forward. You can type forward, left, or right."
+#
+#   user_input = gets.chomp
+#
+#   if user_input == "right"
+#     puts "You were killed by a goblin!"
+#     break
+#   elsif user_input == "left"
+#     puts "You were killed by a werewolf!"
+#     break
+#   elsif user_input == "forward"
+#     if move_number == 2
+#       puts "You win!"
+#       break
+#     end
+#
+#     move_number += 1
+#   end
+# end

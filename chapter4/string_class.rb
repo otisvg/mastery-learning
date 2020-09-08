9502bc465b58d46721b75c4273a6dca42f52d4bb
Otis <otisvickersgraver@gmail.com>
2020-09-08 19:37:14 +0100

    Chapter 5 String class variable assignment

--- chapter4/string_class.rb
+++ chapter4/string_class.rb
@@ -6,3 +6,15 @@ todo_1 = String.new("wash the dog")
 todo_1.capitalize!
 
 puts todo_1
+
+# Assign to the variable one_string a new instance of String, with the text "1".
+# Assign to the variable two_string a new instance of String, with the text "2".
+# Tell one_string to return the result of adding itself to two_string
+# Explain what happens, and why it's different from the 1 + 2 statement we did with instances of Integer.
+# Try adding together a string with an integer. Explain what happens.
+
+one_string = String.new("1")
+two_string = String.new("2")
+
+puts one_string + two_string
+puts one_string + 5
